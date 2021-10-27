454cad325e0c550791fde1463a13edc2f2906b9f
pengyihao <pathology666@163.com>
2021-10-27 14:21:52 +0800

    bug comes from code, but stronger than code

--- utils/validations.js
+++ utils/validations.js
@@ -1,8 +1,8 @@
-function isvaildNum(error, num) {
+function isvaildNum(error, num, useZh) {
     let reg = /^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\d{8}$/;
     let res = reg.test(num);
     if (!res) {
-        error.phoneNumber = "invaild phone number"
+        error.phoneNumber = useZh? "无效的手机号码":"invaild phone number"
     }
 }
 function isvaildidCardNum(error, idCard, useZh) {
