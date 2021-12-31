9e2375d9e2272a3f38bac5a5e469d7072b24e40a
pengyihao <pathology666@163.com>
2021-12-31 16:10:35 +0800

    some database things

--- tests/sequelize_update_return.js
+++ tests/sequelize_update_return.js
@@ -1,24 +1,37 @@
-const {User, RecruitmentRecord, Recruitment} = require('../models')
-const {Op} = require('sequelize')
+const { User, RecruitmentRecord, Recruitment } = require('../models')
+const { Op } = require('sequelize')
+const chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const run = () => {
+  for (let i = 0; i < 100; i++) {
+    let name = ""
+    for (let j = 0; j < 6; j++) {
+      name += chars[Math.round(Math.random() * 26)]
+    }
+    Recruitment.create({
+      name
+    }).then(result => {
+      try {
 
-Recruitment.create({}).then(result => {
-    try {
         RecruitmentRecord.upsert({
+          user_id: 1,
+          recruitment_id: result.id,
+          canceled: false,
+          extra_datas: JSON.stringify({ size: 1 }),
+          is_comp: true,
+        }, {
+          where: {
             user_id: 1,
-            recruitment_id: 3,
-            canceled: false,
-            extra_datas: JSON.stringify({ size: 1 }),
-            is_comp: true,
-          }, {
-            where: {
-              user_id: 1,
-              recruitment_id: 1,
-            },
-            returning: true
-          }).then(res => {
-              console.log(res[0].isNewRecord)
-          })
-        } catch (err) {
-          throw err
-        }
-})
+            recruitment_id: 1,
+          },
+          returning: true
+        }).then(res => {
+          console.log(res[0].isNewRecord)
+        })
+      } catch (err) {
+        throw err
+      }
+    })
+  }
+}
+
+run()
